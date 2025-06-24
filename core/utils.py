9c58c84690a07c7d448723742fa44b5c9d2ff801
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 12:53:18 +0800

    added progress thread in utility

---
+++ core/utils.py
@@ -0,0 +1,12 @@
+import threading
+import time
+
+def start_progress_thread():
+    def update_progress():
+        while True:
+            if not audio_player.paused:
+                update_func()
+            time.sleep(0.1)
+
+    progress_thread = threading.Thread(target=update_progress, daemon=True)
+    progress_thread.start()
