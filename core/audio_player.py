c358dec138fc456caec14ffb8e76d0be87b09d7a
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 12:43:12 +0800

    stop audio

--- core/audio_player.py
+++ core/audio_player.py
@@ -2,19 +2,24 @@ import pygame
 
 class AudioPlayer:
     def __init__(self):
-        self.paused = False
-        self.current_position = 0
+        self._paused = False
+        self._current_position = 0
 
     def play(self, file_path):
         pygame.mixer.music.load(file_path)
         pygame.mixer.music.play()
-        self.paused = False
-        self.current_position = 0
+        self._paused = False
+        self._current_position = 0
 
     def pause(self):
         pygame.mixer.music.pause()
-        self.paused = True
+        self._paused = True
 
     def resume(self):
         pygame.mixer.music.unpause()
-        self.paused = False
+        self._paused = False
+
+    def stop(self):
+        pygame.mixer.music.stop()
+        self._paused = False
+        self._current_position = 0
