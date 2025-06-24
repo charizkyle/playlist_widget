ca7cca2c5239d53c4e64ce58df03e0fdae0db927
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 14:39:47 +0800

    call PlaylistApp in app file to display

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from views.playlist_view import PlaylistApp
 
 class MusicPlayerApp:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title("🎶 Playlist Widget")
+        self.window.title("🎶 Music Player")
         self.window.geometry("600x700")
+
+        self.view = PlaylistApp(self.window)
+        self.view.show()
 
     def run(self):
         self.window.mainloop()
