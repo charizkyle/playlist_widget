02302decc681c8dcb520b11852637d5661d9063b
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 13:15:55 +0800

    :added setup ui

--- views/playlist_view.py
+++ views/playlist_view.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+from tkinter import filedialog
+from tkinter.ttk import Progressbar
 import customtkinter as ctk
+from mutagen.mp3 import MP3
 import pygame
 import os
 from core.audio_player import AudioPlayer
@@ -16,4 +19,7 @@ class PlaylistApp:
         self.setup_ui()
         start_progress_thread(self.update_progress, self.audio)
 
-        
+    def setup_ui(self):
+        self.bg_image = tk.PhotoImage(file="assets/background.png")
+        self.bg_label = tk.Label(self.frame, image=self.bg_image)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
