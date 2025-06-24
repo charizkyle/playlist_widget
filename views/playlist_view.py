ac393385cf9f0a2381b1fb929339979f325d2b10
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 13:03:48 +0800

    playlist app

---
+++ views/playlist_view.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import customtkinter as ctk
+import pygame
+import os
+
+class PlaylistApp:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.audio = AudioPlayer()
+        self.songs = []
+        self.current_index = 0
+        
