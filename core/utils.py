7a6b042f0419b6c6117d3fb4e6208b87bbe509b3
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 18:35:31 +0800

    reduced the thread sleep time in core/utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,13 +1,12 @@
 import threading
 import time
 
-def start_progress_thread(update_func, audio_player):
+def start_progress_thread(audio_player, update_func):
     def update_progress():
         while True:
             if not audio_player.paused:
                 update_func()
-            time.sleep(0.1)
-
+            time.sleep(0.5)
     thread = threading.Thread(target=update_progress)
     thread.daemon = True
     thread.start()
