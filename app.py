3e22d862ee195441472ee551391e19512d8046fc
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 15:03:56 +0800

    changed window size

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ class MusicPlayerApp:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("🎶 Music Player")
-        self.window.geometry("600x700")
+        self.window.geometry("768x768")
+        self.window.resizable(False, False)
 
         self.view = PlaylistApp(self.window)
         self.view.show()
