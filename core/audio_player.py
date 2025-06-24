5ab9dddaaea8c7501c863e0240b8517a49a9ca5f
charizkyle <santoscharizkyle@gmail.com>
2025-06-24 12:38:53 +0800

    audio player, pygame music load

---
+++ core/audio_player.py
@@ -0,0 +1,12 @@
+import pygame
+
+class AudioPlayer:
+    def __init__(self):
+        self.paused = False
+        self.current_position = 0
+
+    def play(self, file_path):
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+        self.paused = False
+        self.current_position = 0
